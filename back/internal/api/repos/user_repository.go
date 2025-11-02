9416ffeb5c3884c21b27b06d3ed0352ef34702c0
Romain CHENARD <rom.chenard@gmail.com>
2025-11-02 20:18:21 +0100

    feat: 🎸 user repo isolated

--- back/internal/api/repos/user_repository.go
+++ back/internal/api/repos/user_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	db "chat/db/sqlc"
 	custom_errors "chat/internal/api/errors"
+	user_management_domain "chat/internal/modules/user-management/domain"
 	"context"
 	"database/sql"
 	"fmt"
@@ -20,23 +21,38 @@ func NewUserRepo() *UserRepository {
 	}
 }
 
-func (userRepo *UserRepository) CreateUser(ctx context.Context, arg db.CreateUserParams) (db.User, error) {
+func (userRepo *UserRepository) CreateUser(ctx context.Context, newUser *user_management_domain.User) error {
+	userSnapshot := newUser.ToSnapshot()
+	arg := db.CreateUserParams{
+		ID:        userSnapshot.ID,
+		Email:     userSnapshot.Email,
+		Password:  userSnapshot.Password,
+		CreatedAt: userSnapshot.CreatedAt,
+		UpdatedAt: userSnapshot.UpdatedAt,
+	}
 	arg.CreatedAt = time.Now()
 	arg.UpdatedAt = arg.CreatedAt
-	user, err := (*userRepo.Store).CreateUser(ctx, arg)
+	_, err := (*userRepo.Store).CreateUser(ctx, arg)
 	if err == sql.ErrNoRows {
-		return db.User{}, custom_errors.NewErrNotFound(err)
+		return custom_errors.NewErrNotFound(err)
 	} else if err != nil {
-		return db.User{}, err
+		return err
 	}
-	return user, nil
+	return nil
 }
 
-func (userRepo *UserRepository) FindUserByEmail(ctx context.Context, email string) (db.User, error) {
+func (userRepo *UserRepository) FindUserByEmail(ctx context.Context, email string) (*user_management_domain.User, error) {
 	foundUser, err := (*userRepo.Store).GetUserByEmail(ctx, email)
 	if err != nil {
 		fmt.Println("find", err)
-		return db.User{}, err
+		return nil, err
 	}
-	return foundUser, nil
+	userSnapshot := user_management_domain.UserSnapshot{
+		ID:        foundUser.ID,
+		Email:     foundUser.Email,
+		Password:  foundUser.Password,
+		CreatedAt: foundUser.CreatedAt,
+		UpdatedAt: foundUser.UpdatedAt,
+	}
+	return user_management_domain.FromSnapshot(userSnapshot), nil
 }
