8f5794f36b8d39b66e9cc2a41319f0bb73aa5c26
Romain CHENARD <rom.chenard@gmail.com>
2025-11-06 19:29:13 +0100

    feat: 🎸 iwebsocket

--- back/internal/modules/chat/domain/client/IWebSocket.go
+++ back/internal/modules/chat/domain/client/IWebSocket.go
@@ -1,7 +1,12 @@
 package chat_client
 
+import "chat/internal/modules/chat/domain/messages"
+
 type IWebSocket interface {
-	WriteTextMessage(data []byte) error
+	WriteTextMessage(message *messages.Message) error
+	WriteInfoMessage(messageType string, content map[string]string) error
+	WriteEvent(event IEvents) error
+	WriteHelloMessage() error
 	WriteCloseMessage() error
 	GetChan() chan (ICommandMessageIn)
 }
