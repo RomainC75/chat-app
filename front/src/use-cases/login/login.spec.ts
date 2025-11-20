3eb888be96d31f8c2211f235ce3ae4e4cb883e4b
Romain CHENARD <rom.chenard@gmail.com>
2025-11-20 19:27:17 +0100

    test: 💍 login raise error when login/pass is wrong

--- front/src/use-cases/login/login.spec.ts
+++ front/src/use-cases/login/login.spec.ts
@@ -1,31 +1,43 @@
-import { FakeAuthGateway } from "../../adapters/secondary/gateways/fakeAuthGateway";
+import { HttpAuthGateway } from "../../adapters/secondary/gateways/httpAuthGateway";
+import { FakeAAuthApiLoader } from "../../adapters/secondary/loaders/fakeAuthLoader";
 import type { AppState } from "../../store/appState";
-import { initReduxStore, type ReduxStore } from "../../store/store"
+import { initReduxStore, type ReduxStore } from "../../store/store";
 import { login } from "./login";
 
-describe("login use-case",()=>{
-    let store: ReduxStore;
-    let authGateway: FakeAuthGateway;
+describe("login use-case", () => {
+  let store: ReduxStore;
+  let authGateway: HttpAuthGateway;
+  let fakeAuthApiLoader: FakeAAuthApiLoader;
 
-    beforeEach(()=>{
-        authGateway = new FakeAuthGateway();
-        store = initReduxStore({gateways: {
-            authGateway
-        }})
-    })
+  beforeEach(() => {
+    fakeAuthApiLoader = new FakeAAuthApiLoader();
+    authGateway = new HttpAuthGateway(fakeAuthApiLoader);
+    store = initReduxStore({
+      gateways: {
+        authGateway,
+      },
+    });
+  });
 
-    it('should login', async ()=>{
-        const email = "bob@email.com"
-        const id = "123"
-        const token = "TOKEN"
-        authGateway.expectedEmail= email
-        authGateway.expectedId = id
-        authGateway.expectedToken = token
+  it("should login", async () => {
+    const email = "bob@email.com";
+    const id = "123";
+    const token = "TOKEN";
+    fakeAuthApiLoader.expectedEmail = email;
+    fakeAuthApiLoader.expectedId = id;
+    fakeAuthApiLoader.expectedToken = token;
 
-        await store.dispatch(login("john", "pass"));
-        console.log("-> store.getState().authManagement")
-        expect(store.getState().authManagement).toEqual<
-      AppState["authManagement"]
-    >({ data: {email, id, token}, error: null });
-    })
-})
+    await store.dispatch(login("john", "pass"));
+    expect(store.getState().authManagement).toEqual<AppState["authManagement"]>(
+      { data: { email, id, token }, error: null }
+    );
+  });
+
+  it("should raise an error if the email/pass is wrong", async () => {
+    fakeAuthApiLoader.shouldRaiseAnError = true;
+    await store.dispatch(login("john", "pass"));
+    expect(store.getState().authManagement).toEqual<AppState["authManagement"]>(
+      { data: { email: "", id: "", token: "" }, error: "wrong email/password" }
+    );
+  });
+});
