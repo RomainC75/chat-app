95cafd0f8a3e58ec3c6ccaeb0af8539d9a84e381
Romain CHENARD <rom.chenard@gmail.com>
2025-11-20 18:02:12 +0100

    feat: 🎸 login use-case

--- front/src/use-cases/login/login.ts
+++ front/src/use-cases/login/login.ts
@@ -1,20 +1,22 @@
 import { createAction } from "@reduxjs/toolkit";
+import type { AppThunk } from "../../store/store";
+import type { TLogin } from "../../core-logic/auth/types/auth.type";
 
-export const answerSubmitted =
-  createAction<AnswerValidation>("ANSWER_SUBMITTED");
+export const userLoggedIn =
+  createAction<TLogin>("LOGGED_ID");
 
 export const alreadyValidated = createAction<void>("ALREADY_VALIDATED");
 
 export const login =
-  (questionId: string, answerLetter: AnswerLetter): AppThunk<Promise<void>> =>
-  async (dispatch, getState, { questionGateway }) => {
-    if (getState().answerValidation.data != null) {
+  (email: string, password: string): AppThunk<Promise<void>> =>
+  async (dispatch, getState, { authGateway }) => {
+    if (getState().authManagement.data != null) {
       dispatch(alreadyValidated());
       return;
     }
-    const answerValidation = await questionGateway.submitAnswer(
-      questionId!,
-      answerLetter,
+    const logValidation = await authGateway.login(
+      email,
+      password,
     );
-    dispatch(answerSubmitted(answerValidation));
+    dispatch(userLoggedIn(logValidation));
   };
