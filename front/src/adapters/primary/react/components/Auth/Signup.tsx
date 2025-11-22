86e10252f366371d1b90e78846252ea9a60707d4
Romain CHENARD <rom.chenard@gmail.com>
2025-11-22 15:01:56 +0100

    feat: 🎸 error message added to Signup

--- front/src/adapters/primary/react/components/Auth/Signup.tsx
+++ front/src/adapters/primary/react/components/Auth/Signup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   Box,
   TextField,
@@ -6,69 +6,72 @@ import {
   Typography,
   Paper,
   Container,
-} from '@mui/material'
+  Alert,
+} from "@mui/material";
+import CheckIcon from '@mui/icons-material/Check';
 
-import { useDispatch, useSelector } from 'react-redux'
-import { type AppDispatch } from '../../../../../store/store';
-import { Link, useNavigate } from 'react-router-dom';
-import { signup } from '../../../../../use-cases/auth/signup/signup';
+import { useDispatch, useSelector } from "react-redux";
+import { type AppDispatch } from "../../../../../store/store";
+import { Link, useNavigate } from "react-router-dom";
+import { signup } from "../../../../../use-cases/auth/signup/signup";
+import type { AppState } from "../../../../../store/appState";
 
 const Signup = () => {
-    const dispach = useDispatch<AppDispatch>();
-    const navigate = useNavigate();
-    const authState = useSelector(
+  const dispach = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const { isLoading, error } = useSelector(
     (state: AppState) => state.authManagement
   );
 
-
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    confirmPassword: ''
-  })
-
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setFormData(prev => ({
+    const { name, value } = e.target;
+    setFormData((prev) => ({
       ...prev,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    if (!validateForm()){
-        console.log("---> password don't match")
-        return
+    e.preventDefault();
+    if (!validateForm()) {
+      console.log("---> password don't match");
+      return;
     }
-    await dispach(signup(formData.email, formData.password))
-    navigate("/login")
-  }
+    try {
+      await dispach(signup(formData.email, formData.password));
+      navigate("/login");
+    } catch  {
+      // console.log("-> error ", error);
+    }
+  };
 
   const validateForm = (): boolean => {
-    if(formData.confirmPassword != formData.password){
-        return false
+    if (formData.confirmPassword != formData.password) {
+      return false;
     }
-    return true
-  }
+    return true;
+  };
 
   return (
     <Container component="main" maxWidth="sm">
       <Box
         sx={{
           marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <Paper elevation={3} sx={{ padding: 4, width: '100%' }}>
+        <Paper elevation={3} sx={{ padding: 4, width: "100%" }}>
           <Typography component="h1" variant="h4" align="center" gutterBottom>
             Sign Up
           </Typography>
-          
-
 
           <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
             <TextField
@@ -82,15 +85,15 @@ const Signup = () => {
               autoFocus
               value={formData.email}
               onChange={handleChange}
-            //   error={!!errors.email}
-            //   helperText={errors.email}
+              //   error={!!errors.email}
+              //   helperText={errors.email}
             />
             <TextField
               margin="normal"
               required
               fullWidth
               name="password"
-              label="Password"
+              label="New Password"
               type="password"
               id="password"
               autoComplete="new-password"
@@ -113,17 +116,23 @@ const Signup = () => {
               type="submit"
               fullWidth
               variant="contained"
+              role="confirm"
               sx={{ mt: 3, mb: 2 }}
               disabled={isLoading}
             >
-              {isLoading ? 'Signing Up...' : 'Sign Up'}
+              {isLoading ? "Signing Up..." : "Sign Up"}
             </Button>
+            {error ?? (
+              <Alert icon={<CheckIcon fontSize="inherit" />} severity="error">
+                error
+              </Alert>
+            )}
             <Link to="/login">Login</Link>
           </Box>
         </Paper>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
